f1098666885e1676b9d9d5ec7d851da7872870a1
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-01-24 09:02:56 +0700

    update model

--- app/src/components/dashboard.js
+++ app/src/components/dashboard.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css';
-import Table from 'react-bootstrap/Table';
-import Card from 'react-bootstrap/Card';
+import { Card, Table, Button, Modal, Form } from 'react-bootstrap';
+
+import UseFetch from '../hooks/useFetch';
+
+
 function Dashboard() {
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [editedData, setEditedData] = useState({ base: '', counter: '', rate: '' });
+
+    const {data,getData} = UseFetch()
+    useEffect(() => {
+        if (data.length === 0) {
+          const fetchData = async () => {
+            await getData();
+          };
+    
+          fetchData();
+        }
+      }, [getData, data]);
+
+
+      const handleEditClick = (item) => {
+        setSelectedItem(item);
+        setEditedData({ base: item.base, counter: item.counter, rate: item.rate });
+        setShowEditModal(true);
+      };
+
+
+      const handleSaveChanges = () => {
+        console.log(selectedItem)
+     
+      
+        setShowEditModal(false);
+      };
+
     return (
         <div className='container mt-5'>
         <Card border="primary" style={{ width: '60rem' }}>
@@ -19,20 +52,70 @@ function Dashboard() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                      <td><button className='btn btn-danger btn-sm'>Delete</button><button className='btn btn-primary btn-sm mx-2'>Edit</button></td>
+                    {data.map((item) =>
+                         <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.base}</td>
+                      <td>{item.counter}</td>
+                      <td>{item.rate}</td>
+                      <td><button className='btn btn-danger btn-sm' >Delete</button><button className='btn btn-primary btn-sm mx-2' onClick={() => handleEditClick(item)}>Edit</button></td>
                     </tr>
+                    )}
+                   
                  
                   </tbody>
                 </Table>
                 </Card.Body>
               </Card>
               
-               
+        <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Data</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBase">
+              <Form.Label>Base</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter base"
+                value={editedData.base}
+                onChange={(e) => setEditedData({ ...editedData, base: e.target.value })}
+
+              />
+            </Form.Group>
+            <Form.Group controlId="formCounter">
+              <Form.Label>Counter</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter counter"
+                value={editedData.counter}
+                onChange={(e) => setEditedData({ ...editedData, counter: e.target.value })}
+
+              />
+            </Form.Group>
+            <Form.Group controlId="formRate">
+              <Form.Label>Rate</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter rate"
+                value={editedData.rate}
+                onChange={(e) => setEditedData({ ...editedData, rate: e.target.value })}
+
+                
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowEditModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSaveChanges}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
                
           </div>
           );
--- app/src/hooks/useFetch.js
+++ app/src/hooks/useFetch.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function UseFetch() {
   const [data, setData] = useState([]);
 
   async function getData() {
     try {
-      const response = await fetch('https://djangodrf.arifsultani291.digital/products/');
+      const response = await fetch('http://127.0.0.1:8000/currencies/add/', {
+        method: 'GET',
+        // Headers should not include 'Access-Control-Allow-Origin'
+        headers: {
+          'Content-Type': 'application/json',
+          // Add other headers as needed
+        },
+      });
 
       if (!response.ok) {
         console.error('Failed to fetch data. Server responded with:', response.statusText);
@@ -14,7 +21,7 @@ function UseFetch() {
 
       const jsonData = await response.json();
       setData(jsonData);
-      console.log(jsonData)
+      console.log(jsonData);
     } catch (error) {
       console.error('An error occurred while fetching data:', error.message);
     }
